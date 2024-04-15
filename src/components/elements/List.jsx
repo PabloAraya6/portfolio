50fd529760c420569d96880b82b588d931bf8c8d
Pablo Araya <arayap812@gmail.com>
2024-04-14 21:27:21 -0300

    feat: improve timeline design

--- src/components/elements/List.jsx
+++ src/components/elements/List.jsx
@@ -10,29 +10,27 @@ const List = ({ list, key }) => {
   }
 
   return (
-    <div>
-      <div key={key} className='timeline-container'>
+    <div key={key} className='timeline-container'>
+      <div
+        className='timeline-block'
+        onClick={() => toggleDetails(`details-${key}`)}
+      >
+        <h2>{list.name.split(' at ')[0]}</h2>
+        <span className='timeline-date'>{list.name.split(' at ')[1]}</span>
         <div
-          className='timeline-block'
-          onClick={() => toggleDetails(`details-${key}`)}
+          id={`details-${key}`}
+          className='details'
+          style={{
+            display: visibleDetail === `details-${key}` ? 'block' : 'none',
+          }}
         >
-          <h2>{list.name.split(' at ')[0]}</h2>
-          <span className='timeline-date'>{list.name.split(' at ')[1]}</span>
-          <div
-            id={`details-${key}`}
-            className='details'
-            style={{
-              display: visibleDetail === `details-${key}` ? 'block' : 'none',
-            }}
-          >
-            {list.items.map((item, idx) => (
-              <p key={idx}>
-                <Icon icon={item.icon} />
-                &nbsp;
-                {item.name}
-              </p>
-            ))}
-          </div>
+          {list.items.map((item, idx) => (
+            <p key={idx}>
+              <Icon icon={item.icon} />
+              &nbsp;
+              {item.name}
+            </p>
+          ))}
         </div>
       </div>
     </div>
