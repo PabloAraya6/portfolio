c3a7e0bca93b16e57dd60b74405833839637bf97
Pablo Araya <arayap812@gmail.com>
2025-03-07 22:52:38 -0300

    chore: add mobile padding and calculated YOE

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,6 +2,16 @@ import '/src/stylesheets/sections/Home.css'
 import SocialLinks from '/src/components/elements/SocialLinks'
 
 const Home = () => {
+  const calculateYearsOfExperience = () => {
+    const startDate = new Date(2019, 10)
+    const currentDate = new Date()
+    const yearsDiff = currentDate.getFullYear() - startDate.getFullYear()
+    const monthsDiff = currentDate.getMonth() - startDate.getMonth()
+    return yearsDiff + monthsDiff / 12
+  }
+
+  const yearsOfExperience = Math.floor(calculateYearsOfExperience())
+
   return (
     <section className='Home section' id='home'>
       <div className='container'>
@@ -10,7 +20,8 @@ const Home = () => {
           <h1 className='Home-h1'>Pablo Araya</h1>
           <h2 className='Home-h2'>Software Developer</h2>
           <p className='Home-description'>
-            With over 4 years of experience in software development,
+            With over {yearsOfExperience} years of experience in software
+            development,
             <br /> I am dedicated to pushing the limits of technology to create
             impactful solutions.
           </p>
